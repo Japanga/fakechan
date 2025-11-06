f8c9424240becbfcb56ee4defda127c3d11ecf4c
Japanga <japanganemo@gmail.com>
2025-11-06 15:02:04 -0500

    Update colortext.js

--- boards/colortext.js
+++ boards/colortext.js
@@ -1,11 +1,10 @@
-          <script>
-document.addEventListener("DOMContentLoaded", function() {
-  const wordToReplace = "$DESU$";
+// colortext.js
+function sayHello(name) {
+   const wordToReplace = "$DESU$";
   const replacementHTML = '<h1 class="desu-text">$DESU$</h1>';
   document.body.innerHTML = document.body.innerHTML.replaceAll(wordToReplace, replacementHTML);
     const wordToReplace2 = "$DOUBLERAINBOW$";
    const replacementHTML2 = '<h1 class="rainbow-text">$DOUBLERAINBOW$</h1>';
   document.body.innerHTML =
   document.body.innerHTML.replaceAll(wordToReplace2, replacementHTML2); 
-};
-  </script> 
+}          
