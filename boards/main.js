73b5c660a22bbe439b5487f30a586b75749bc409
Japanga <japanganemo@gmail.com>
2025-11-06 15:33:18 -0500

    Update main.js

--- boards/main.js
+++ boards/main.js
@@ -5,6 +5,3 @@ import { colorTexting } from './colortext.js'; // Adjust the path as necessary
 // Call the imported function immediately (optional)
 colorTexting();
 
-// Call the imported function repeatedly every 3 seconds (3000 milliseconds)
-const intervalId = setInterval(colorTexting, 3000);
-
