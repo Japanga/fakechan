ea715ee56f7026860314d07361dc3f6a8ccd5f9c
Japanga <japanganemo@gmail.com>
2025-12-13 16:38:25 -0500

    Update boardlinks.js

--- boards/boardlinks.js
+++ boards/boardlinks.js
@@ -10,7 +10,30 @@ const linksData = [
     { text: "/b/", url: "https://japanga.github.io/fakechan/boards/b" },
     { text: "/co/", url: "https://japanga.github.io/fakechan/boards/co" },
      { text: "/c/", url: "https://japanga.github.io/fakechan/boards/c" },
-     { text: "/ck/", url: "https://japanga.github.io/fakechan/boards/ck" }
+     { text: "/ck/", url: "https://japanga.github.io/fakechan/boards/ck" },
+     { text: "/dbs/", url: "https://japanga.github.io/fakechan/boards/dbs" },
+     { text: "/get/", url: "https://japanga.github.io/fakechan/boards/get" },
+     { text: "/gif/", url: "https://japanga.github.io/fakechan/boards/gif" },
+     { text: "/h/", url: "https://japanga.github.io/fakechan/boards/h" },
+     { text: "/hc/", url: "https://japanga.github.io/fakechan/boards/hc" },
+     { text: "/hyb/", url: "https://japanga.github.io/fakechan/boards/hyb" },
+     { text: "/lit/", url: "https://japanga.github.io/fakechan/boards/lit" },
+     { text: "/x/", url: "https://japanga.github.io/fakechan/boards/x" },
+     { text: "/m/", url: "https://japanga.github.io/fakechan/boards/m" },
+     { text: "/mu/", url: "https://japanga.github.io/fakechan/boards/mu" },
+     { text: "/v/", url: "https://japanga.github.io/fakechan/boards/v" },
+     { text: "/vid/", url: "https://japanga.github.io/fakechan/boards/vid" },
+     { text: "/tv/", url: "https://japanga.github.io/fakechan/boards/tv" },
+     { text: "/pol/", url: "https://japanga.github.io/fakechan/boards/pol" },
+     { text: "/p/", url: "https://japanga.github.io/fakechan/boards/p" },
+     { text: "/qa/", url: "https://japanga.github.io/fakechan/boards/qa" },
+     { text: "/sp/", url: "https://japanga.github.io/fakechan/boards/sp" },
+     { text: "/jp/", url: "https://japanga.github.io/fakechan/boards/jp" },
+     { text: "/w/", url: "https://japanga.github.io/fakechan/boards/w" },
+     { text: "/wg/", url: "https://japanga.github.io/fakechan/boards/wg" },
+     { text: "/wsg/", url: "https://japanga.github.io/fakechan/boards/wsg" },
+     { text: "/pw/", url: "https://japanga.github.io/fakechan/boards/pw" },
+     { text: "/news/", url: "https://japanga.github.io/fakechan/boards/news" }
 ];
 
 // Get the target div element by its ID
